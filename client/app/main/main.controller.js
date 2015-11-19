77b0bbe51a16afbf56c5dbc9450430973e3a6440
aviadh <aviadhahami@gmail.com>
2015-11-19 13:23:21 +0200

    Cleared unsued shit

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('hackIdc2016App')
-  .controller('MainCtrl', ['$scope',function ($scope) {
-
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.video = {
+      id: "il9nqWw9W3Y"
+    }
   }]);
