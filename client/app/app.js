7d4c488ca0ba2fd2272ebd9bde9eea1d8ac3c7bf
aviadh <aviadhahami@gmail.com>
2015-11-19 16:40:13 +0200

    Added fullPage

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,8 @@ angular.module('hackIdc2016App', [
   'ngMessages',
   'btford.socket-io',
   'ui.router',
-  'ngMaterial'
+  'ngMaterial',
+  'br.fullpage'
 ])
   .config(function($mdIconProvider) {
     $mdIconProvider
