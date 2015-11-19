c4c2ee69e08c4e411feb8d40e170e62a5eda34e4
aviadh <aviadhahami@gmail.com>
2015-11-19 13:23:12 +0200

    Killed dep

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,8 @@ angular.module('hackIdc2016App', [
   'ngMessages',
   'btford.socket-io',
   'ui.router',
-  'ngMaterial'
+  'ngMaterial',
+  'angularVideoBg'
 ])
   .config(function($mdIconProvider) {
     $mdIconProvider
