df05bdaf8b3a9ea0dd5daa9a3f1ae94693730176
Aviad Hahami <aviadhahami@gmail.com>
2016-01-06 01:43:02 +0200

    Header scroll directive

---
+++ client/app/headerScroll/headerScroll.directive.js
@@ -0,0 +1,23 @@
+'use strict';
+//'myModal', ['$log', function ($log, $window) {
+angular.module('hackIdcApp')
+  .directive('headerScroll', ['$window',function ($window) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        angular.element($window).bind("scroll", function () {
+          if (!scope.scrollPosition) {
+            scope.scrollPosition = 0
+          }
+
+          if (this.pageYOffset > scope.scrollPosition) {
+            scope.boolChangeClass = true;
+          } else {
+            scope.boolChangeClass = false;
+          }
+          scope.scrollPosition = this.pageYOffset;
+          scope.$apply();
+        });
+      }
+    }}
+  ]);
