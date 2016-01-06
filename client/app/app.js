c0ebf0d8e86c170b996c2cccd8ec13da12d9746f
Aviad Hahami <aviadhahami@gmail.com>
2016-01-06 03:31:05 +0200

    work work

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,8 @@ angular.module('hackIdcApp', [
   'ngMessages',
   'ui.router',
   'ngMaterial',
-  'duScroll'
+  'duScroll',
+  'ng-mfb'
 ])
   .config(function($mdIconProvider) {
     $mdIconProvider
