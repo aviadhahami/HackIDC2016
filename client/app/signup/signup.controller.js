6aef0aacac5ce8b343d37453b4dc9ee7e15b8654
Aviad Hahami <aviadhahami@gmail.com>
2016-01-08 19:01:34 +0200

    Fixesd date

--- client/app/signup/signup.controller.js
+++ client/app/signup/signup.controller.js
@@ -5,6 +5,9 @@ angular.module('hackIdcApp')
   .controller('SignupCtrl',  ['$scope','isMobile',
       function ($scope,isMobile) {
         $scope.isMobile = isMobile.isMobile();
+        $scope.timeToCount = (function(){
+          return Math.round((new Date('02/01/2016') -new Date())/1000);
+        })();
       }
     ]
   );
