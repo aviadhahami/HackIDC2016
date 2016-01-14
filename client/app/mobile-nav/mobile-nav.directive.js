e48e8a41b76371dda3c2ed67afdd136c3232de3e
Aviad Hahami <aviadhahami@gmail.com>
2016-01-14 09:45:56 +0200

    Working on styling the grid

--- client/app/mobile-nav/mobile-nav.directive.js
+++ client/app/mobile-nav/mobile-nav.directive.js
@@ -1,14 +1,29 @@
 'use strict';
 
 angular.module('hackIdcApp')
-  .directive('mobileNav', function () {
+  .directive('mobileNav', ['$document',function ($document) {
     return {
       templateUrl: 'app/mobile-nav/mobile-nav.html',
       restrict: 'E',
       link: function (scope, element, attrs) {
         var links = attrs.links;
-       scope.bgColor = attrs.triggerFabColor || 'rgb(255,87,34)';
-        console.log(scope)
+        var gridContainer = angular.element(element[0].querySelector('#grid-bg'));
+        var pageBody = angular.element($document[0].body);
+
+        scope.bgColor = attrs.triggerFabColor || 'rgb(255,87,34)';
+        scope.bgGridColor = attrs.bgGridColor || 'rgba(0,0,0,0.5)';
+
+
+
+        scope.displayGrid = function(){
+
+          gridContainer.css('display','block');
+          pageBody.css('overflow','hidden');
+        };
+        scope.hideGrid = function(){
+          gridContainer.css('display','none');
+          pageBody.css('overflow','auto');
+        };
       }
     };
-  });
+  }]);
