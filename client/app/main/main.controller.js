cfd393ce597f58ed4dc2845fa16e393486146333
Aviad Hahami <aviadhahami@gmail.com>
2016-01-15 18:02:40 +0200

    almost  done mentorship

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -18,7 +18,11 @@ angular.module('hackIdcApp')
           mentorship :'Mentorship'
         };
         $scope.content= {
-          descriptionContent : "HackIDC is the first national student hackathon in Israel. It is the biggest hackathon in Israel and it’s run by students from the IDC. This hackathon is an event that brings together software,hardware, and design enthusiasts to create new and innovative applications or services from scratch in 36 hours. It is an opportunity to meet and learn from people from different backgrounds and schools and an opportunity to learn from mentors that come from the top companies in the high tech industry in Israel. Ultimately it is a fun & professional experience."
+          descriptionContent : "HackIDC is the first national student hackathon in Israel. It is the biggest hackathon in Israel and it’s run by students from the IDC. This hackathon is an event that brings together software,hardware, and design enthusiasts to create new and innovative applications or services from scratch in 36 hours. It is an opportunity to meet and learn from people from different backgrounds and schools and an opportunity to learn from mentors that come from the top companies in the high tech industry in Israel. Ultimately it is a fun & professional experience.",
+          mentorshipContent:{
+            A : ' HackIDC will operate a special mentorship program, targeting those who want to participate in a hackathon but still needs to work on their development skills.',
+            B :'  The program\'s goal is to provide the participants with the tools necessary to build a prototype, in the timeframe of a hackathon.To achieve that we will host several sessions with top developers from hi­tech companies, some sessions will be dedicated to provide the participants with practical knowledge, while others will focus on hackathons in general, and how to prototype correctly.Once the hackathon begins, onsite mentors will be available to provide additional assistance and guidance.'
+          }
         };
       },1000);
 
