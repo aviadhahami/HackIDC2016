cb5df470950eb9452c1cf57ff4b22f51469be3d7
Aviad Hahami <aviadhahami@gmail.com>
2016-01-15 23:35:10 +0200

    Added schools

--- client/app/signup/signup.controller.js
+++ client/app/signup/signup.controller.js
@@ -13,7 +13,37 @@ angular.module('hackIdcApp')
           actionArgs: 'home',
           bgColor:'#2185D5'
         }];
-
+        $scope.schools =[
+          "IDC Herzliya",
+          "Academic College of Tel Aviv-Yafo",
+          "Afeka College of Engineering",
+          "Ariel University",
+          "Ashkelon Academic College",
+          "Bar-Ilan University",
+          "Ben-Gurion University of the Negev",
+          "Bezalel Academy of Art and Design",
+          "College of Management Academic Studies",
+          "Hebrew University of Jerusalem",
+          "Jerusalem College of Engineering",
+          "Max Stern Academic College of Emek Yezreel",
+          "Netanya Academic College",
+          "Oranim Academic College",
+          "Ono Academic College",
+          "Open University of Israel",
+          "ORT Braude College of Engineering",
+          "Sapir Academic College",
+          "Sami Shamoon College of Engineering",
+          "Shenkar College of Engineering and Design",
+          "Technion - Israel Institute of Technology",
+          "Tel Aviv University",
+          "Tel-Hai Academic College",
+          "University of Haifa",
+          "Western Galilee College",
+          "WIZO Haifa Academy of Design and Education",
+          "Weizmann Institute of Science",
+          "Avni Institute of Art and Design",
+          "Other"
+        ];
 
         $scope.isMobile = isMobile.isMobile();
         $scope.timeToCount = (function(){
