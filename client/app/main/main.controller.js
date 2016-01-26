c68dacc1d5b879df72693fe06c4f01819c747553
Aviad Hahami <aviadhahami@gmail.com>
2016-01-26 02:27:23 +0200

    Working on rejects from AWWWYEAAA

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -72,30 +72,8 @@ angular.module('hackIdcApp')
           'Once the hackathon begins, onsite mentors will be available to provide additional assistance and guidance.'
         },
         {
-          title:'Jesus',
-          content:' Sé que mis rimas a veces causan disgustos,' +
-          'Cuando mis neuronas corren' +
-          'Hasta yo mismo me asusto' +
-          'Mis respuestas pueden ser tan agresivas,' +
-          'Que hasta las letras me huyen' +
-          'Porque tienen miedo de que las escriba' +
-          'No tengo rifles pa\' matarte' +
-          'Solo basta con la pista' +
-          'Convierto letras en ideas como un ilusionista' +
-          'En una línea te mato, te fracturo, te lesiono' +
-          'Y en la siguiente te resucito cuando te menciono' +
-          'Eso es parte de mi arte' +
-          'Que todo el mundo sepa,' +
-          'Que estas rimas son pa\'' +
-          'ti sin tener que mencionarte' +
-          'Y no lo hago para evitar el roce' +
-          'Lo hago para no hacerte famoso' +
-          'En los países donde nadie te conoce' +
-          'Después de ver como se mueven las guerras y las guerrillas' +
-          'Tu crees que le voy a tener miedo a tu pandilla?' +
-          'Dispara cuando quieras raperito maleantoso,' +
-          'Aquí no gana el mas maleante' +
-          'Gana el mas ingenioso'
+          title:'Got more questions?',
+          content:'Contact us here <a href="mailto:contact@hackidc.com">contact@hackidc.com</a>'
         }
       ];
 
