76c34d1a39c3ec2dc812146c248c8b03da90306f
Aviad Hahami <aviadhahami@gmail.com>
2016-01-31 12:03:31 +0200

    Changed date to Thursday

--- client/app/signup/signup.controller.js
+++ client/app/signup/signup.controller.js
@@ -6,7 +6,7 @@ angular.module('hackIdcApp')
       function ($scope,isMobile,$rootScope,$location,$mdDialog,Upload,$http,$timeout) {
 
 
-        $scope.timeToGo = (new Date('12:00 January 31 2016') - new Date())/1000;
+        $scope.timeToGo = (new Date('12:00 February 4 2016') - new Date())/1000;
 
 
         var fileUploadConfig = "";
