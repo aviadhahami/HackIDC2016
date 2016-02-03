90bf5bfc29501f90cdfc603ded186648ba37ec9c
Aviad Hahami <aviadhahami@gmail.com>
2016-02-03 22:56:56 +0200

    Changed and and small css fix for images stretch

--- client/app/signup/signup.controller.js
+++ client/app/signup/signup.controller.js
@@ -6,7 +6,7 @@ angular.module('hackIdcApp')
       function ($scope,isMobile,$rootScope,$location,$mdDialog,Upload,$http,$timeout) {
 
 
-        $scope.timeToGo = (new Date('12:00 February 4 2016') - new Date())/1000;
+        $scope.timeToGo = (new Date('12:00 February 13 2016') - new Date())/1000;
 
 
         var fileUploadConfig = "";
