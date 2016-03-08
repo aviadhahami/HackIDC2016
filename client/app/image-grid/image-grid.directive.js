f566c8b46e2b658807cc8d9e979761e0159d7412
Aviad Hahami <aviadhahami@gmail.com>
2016-03-08 17:42:54 +0200

    bounded directive, need to style

--- client/app/image-grid/image-grid.directive.js
+++ client/app/image-grid/image-grid.directive.js
@@ -1,15 +1,16 @@
 'use strict';
 
 angular.module('hackIdcApp')
-  .directive('ImageGrid', [function () {
+  .directive('imageGrid', [function () {
     return {
-      templateUrl: 'app/mobile-nav/image-grid.html',
+      templateUrl: 'app/image-grid/image-grid.html',
       restrict: 'E',
       scope:{
         data: '='
       },
       link: function (scope, element, attrs) {
-
+        console.log(scope.data);
+        console.log('run')
       }
     };
   }]);
