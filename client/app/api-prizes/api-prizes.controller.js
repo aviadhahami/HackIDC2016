c6ee72f7e5a82d63c6f921857c8318408d804aec
Aviad Hahami <aviadhahami@gmail.com>
2016-03-23 01:15:01 +0200

    binding scope

--- client/app/api-prizes/api-prizes.controller.js
+++ client/app/api-prizes/api-prizes.controller.js
@@ -2,6 +2,7 @@
  * Created by aviad on 3/23/2016.
  */
 angular.module('hackIdcApp')
-  .controller('apiPrizesCtrlr',[function(){
+  .controller('apiPrizesCtrlr',['$scope',
+    function($scope){
     $scope.test = "123";
   }]);
