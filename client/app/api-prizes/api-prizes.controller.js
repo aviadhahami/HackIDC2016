9a125ba5873fd4a4caea5db7bec47a0521f58027
Aviad Hahami <aviadhahami@gmail.com>
2016-03-23 02:40:50 +0200

    working nicely

--- client/app/api-prizes/api-prizes.controller.js
+++ client/app/api-prizes/api-prizes.controller.js
@@ -13,11 +13,18 @@ angular.module('hackIdcApp')
       var apis = [
         {
           title:'RapidAPI',
+          companyName:'RapidAPI',
           info:'To be announced',
-          link:'http://google.com/',
+          link:'',
           logo:'../../assets/logos/rapid-api-logo.png'
         },
-        {},
+        {
+          title:'Roundtrip flight ticket to Berlin',
+          companyName:'Backand',
+          info:'Backand challenge is simple! All you need to do is to use our service as the Backand for your project and you can enter the challenge!<br>The winner team will be the one who made the most use and complicated implementation of Backand. Bonus points will be given to each main feature Backand has to offer (use of real-time, actions, queries, hosting and much more)',
+          link:'http://www.backand.com',
+          logo:'../../assets/logos/backand-logo.png'
+        },
         {},
         {}
       ];
