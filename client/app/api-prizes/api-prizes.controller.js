8a2fde71012ad3091fccd67a95903c73912a0285
Aviad Hahami <aviadhahami@gmail.com>
2016-03-23 02:10:20 +0200

    working on page

--- client/app/api-prizes/api-prizes.controller.js
+++ client/app/api-prizes/api-prizes.controller.js
@@ -2,8 +2,25 @@
  * Created by aviad on 3/23/2016.
  */
 angular.module('hackIdcApp')
-  .controller('apiPrizesCtrlr',['$scope',
-    function($scope){
+  .controller('apiPrizesCtrlr',['$scope','isMobile',
+    function($scope,isMobile){
+      $scope.isMobile = isMobile.isMobile();
+
+      $scope.pageDescription='';
 
 
-  }]);
+      $scope.cardWidthClass = !$scope.isMobile ? 'seventy-present-width' : 'fullWidth';
+      var apis = [
+        {
+          title:'RapidAPI',
+          info:'To be announced',
+          link:'http://google.com/',
+          logo:'../../assets/logos/rapid-api-logo.png'
+        },
+        {},
+        {},
+        {}
+      ];
+      $scope.apis = apis;
+
+    }]);
