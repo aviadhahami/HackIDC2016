9753827a9b03b9f2a05edf254f79aa24e0d6155e
Aviad Hahami <aviadhahami@gmail.com>
2016-03-28 23:33:08 +0300

    working on making circles spread across

--- client/app/image-grid/image-grid.directive.js
+++ client/app/image-grid/image-grid.directive.js
@@ -1,25 +1,20 @@
 'use strict';
 
 angular.module('hackIdcApp')
-  .directive('imageGrid', ['isMobile','sponsorsApiGetter','$interval',function (isMobile,sponsorsApiGetter,$interval) {
-    return {
-      templateUrl: 'app/image-grid/image-grid.html',
-      restrict: 'E',
-      scope:{
-        data: '='
-      },
-      link: function (scope, element, attrs) {
-        scope.isMobile = isMobile.isMobile();
-        scope.sponsorsData = sponsorsApiGetter.getData();
+  .directive('imageGrid', ['isMobile','sponsorsApiGetter',
+    function (isMobile,sponsorsApiGetter) {
+      return {
+        templateUrl: 'app/image-grid/image-grid.html',
+        restrict: 'E',
+        scope:{
+          data: '='
+        },
+        link: function (scope, element, attrs) {
+          scope.isMobile = isMobile.isMobile();
+          scope.sponsorsData = sponsorsApiGetter.getData();
 
-        scope.calcFlexSize = scope.isMobile ? 0 : 20;
+          scope.calcFlexSize = scope.isMobile ? 0 : 20;
 
-        $interval(function(){
-          scope.sponsorsData.push({
-            companyName:Math.floor((Math.random() * 10) + 1),
-            prizeTitle:Math.floor((Math.random() * 100) + 1)
-          });
-        },1550);
-      }
-    };
-  }]);
+        }
+      };
+    }]);
