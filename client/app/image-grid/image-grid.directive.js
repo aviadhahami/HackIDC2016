2e61794b2ad2f1c68b9d8d48b6239589a7cc0b73
Aviad Hahami <aviadhahami@gmail.com>
2016-03-28 23:02:28 +0300

    GRID IS WORKING

--- client/app/image-grid/image-grid.directive.js
+++ client/app/image-grid/image-grid.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('hackIdcApp')
-  .directive('imageGrid', ['isMobile','sponsorsApiGetter',function (isMobile,sponsorsApiGetter) {
+  .directive('imageGrid', ['isMobile','sponsorsApiGetter','$interval',function (isMobile,sponsorsApiGetter,$interval) {
     return {
       templateUrl: 'app/image-grid/image-grid.html',
       restrict: 'E',
@@ -13,6 +13,13 @@ angular.module('hackIdcApp')
         scope.sponsorsData = sponsorsApiGetter.getData();
 
         scope.calcFlexSize = scope.isMobile ? 0 : 20;
+
+        $interval(function(){
+          scope.sponsorsData.push({
+            companyName:Math.floor((Math.random() * 10) + 1),
+            prizeTitle:Math.floor((Math.random() * 100) + 1)
+          });
+        },1550);
       }
     };
   }]);
