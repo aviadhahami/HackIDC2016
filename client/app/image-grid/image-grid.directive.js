23d25cc95d301e5931b1d0925f56c87b0665efb8
Aviad Hahami <aviadhahami@gmail.com>
2016-04-22 01:53:42 +0300

    editing the grid

--- client/app/image-grid/image-grid.directive.js
+++ client/app/image-grid/image-grid.directive.js
@@ -9,49 +9,47 @@ angular.module('hackIdcApp')
         scope:{
           data: '='
         },
-      link: function (scope, element, attrs) {
-        scope.sponsorsData = sponsorsApiGetter.getData();
-        scope.isMobile = isMobile.isMobile();
+        link: function (scope, element, attrs) {
+          scope.sponsorsData = sponsorsApiGetter.getData();
+          scope.isMobile = isMobile.isMobile();
 
           scope.gridOpts = {
-            columns:scope.sponsorsData.length*2,
-            width:'auto',
+            columns: 6, // the width of the grid, in columns
+            pushing: true, // whether to push other items out of the way on move or resize
+            floating: true, // whether to automatically float items up so they stack (you can temporarily disable if you are adding unsorted items with ng-repeat)
+            swapping: false, // whether or not to have items of the same size switch places instead of pushing down if they are the same size
+            width: 'auto', // can be an integer or 'auto'. 'auto' scales gridster to be the full width of its containing element
             colWidth: 'auto', // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
-            rowHeight: 'match',
-            margins: [10, 10],
-            outerMargin: true,
-            isMobile:  scope.isMobile,
+            rowHeight: 'match', // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
+            margins: [10, 10], // the pixel distance between each widget
+            outerMargin: true, // whether margins apply to outer edges of the grid
+            isMobile: false, // stacks the grid items if true
             mobileBreakPoint: 600, // if the screen is not wider that this, remove the grid layout and stack the items
             mobileModeEnabled: true, // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
             minColumns: 1, // the minimum columns the grid must have
             minRows: 2, // the minimum height of the grid, in rows
+            maxRows: 100,
             defaultSizeX: 2, // the default width of a gridster item, if not specifed
-            defaultSizeY: 1, // the default height of a gridster item, if not specified
+            defaultSizeY: 2, // the default height of a gridster item, if not specified
             minSizeX: 1, // minimum column width of an item
             maxSizeX: null, // maximum column width of an item
-            minSizeY: 1, // minimum row height of an item
+            minSizeY: 1, // minumum row height of an item
             maxSizeY: null, // maximum row height of an item
-            swapping: false,
             resizable: {
               enabled: false,
               handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
               start: function(event, $element, widget) {}, // optional callback fired when resize is started,
               resize: function(event, $element, widget) {}, // optional callback fired when item is resized,
               stop: function(event, $element, widget) {} // optional callback fired when item is finished resizing
+            },
+            draggable: {
+              enabled: false, // whether dragging items is supported
+              handle: '.my-class', // optional selector for resize handle
+              start: function(event, $element, widget) {}, // optional callback fired when drag is started,
+              drag: function(event, $element, widget) {}, // optional callback fired when item is moved,
+              stop: function(event, $element, widget) {} // optional callback fired when item is finished dragging
             }
           };
-
-
-
-
-        for (var i=0;i<scope.sponsorsData.length;i++){
-
-          scope.sponsorsData[i].sizeX = '1';
-          scope.sponsorsData[i].sizeY = '1';
-          // scope.sponsorsData[i].row = i;
-          // scope.sponsorsData[i].col = '0';
-        }
-
 
           scope.tiles = {
             1: false,
