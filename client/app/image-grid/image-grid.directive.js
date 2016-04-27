d0c6ef30f82e4a895b0c5fc002b496362978cb96
Aviad Hahami <aviadhahami@gmail.com>
2016-04-27 22:58:23 +0300

    nice looking

--- client/app/image-grid/image-grid.directive.js
+++ client/app/image-grid/image-grid.directive.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('hackIdcApp')
-  .directive('imageGrid', ['isMobile','sponsorsApiGetter',
-    function (isMobile,sponsorsApiGetter) {
+  .directive('imageGrid',['isMobile','sponsorsApiGetter','$timeout',
+    function (isMobile,sponsorsApiGetter,$timeout) {
       return {
         templateUrl: 'app/image-grid/image-grid.html',
         restrict: 'E',
@@ -10,10 +10,20 @@ angular.module('hackIdcApp')
           data: '='
         },
         link: function (scope, element, attrs) {
+          var isoOpts = {
+            percentPosition: true
+          };
+          scope.$emit('iso-option', isoOpts);
           scope.sponsorsData = sponsorsApiGetter.getData();
           scope.isMobile = isMobile.isMobile();
           scope.bricks=  scope.sponsorsData;
 
+          scope.toggle = function(brick){
+            brick.toggle = !brick.toggle;
+            $timeout(
+              scope.$emit('iso-method', {name:'layout', params:null})
+              ,0);
+          }
         }
 
       };
