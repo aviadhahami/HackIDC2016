fb9099db5774a0efc453f2c657acdb9997243d2b
Aviad Hahami <aviadhahami@gmail.com>
2016-04-27 23:06:23 +0300

    wtf? no animation

--- client/app/image-grid/image-grid.directive.js
+++ client/app/image-grid/image-grid.directive.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('hackIdcApp')
-  .directive('imageGrid',['isMobile','sponsorsApiGetter','$timeout',
-    function (isMobile,sponsorsApiGetter,$timeout) {
+  .directive('imageGrid',['isMobile','sponsorsApiGetter',
+    function (isMobile,sponsorsApiGetter) {
       return {
         templateUrl: 'app/image-grid/image-grid.html',
         restrict: 'E',
@@ -20,9 +20,7 @@ angular.module('hackIdcApp')
 
           scope.toggle = function(brick){
             brick.toggle = !brick.toggle;
-            $timeout(
-              scope.$emit('iso-method', {name:'layout', params:null})
-              ,0);
+            scope.$emit('iso-method', {name:'layout', params:null})
           }
         }
 
