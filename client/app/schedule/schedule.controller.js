cce0d86ecc01b24c0ea40217116176ba76a9f33f
Aviad Hahami <aviadhahami@gmail.com>
2016-05-03 00:30:11 +0300

    updated sched

--- client/app/schedule/schedule.controller.js
+++ client/app/schedule/schedule.controller.js
@@ -8,22 +8,89 @@ angular.module('hackIdcApp')
     function ($scope,isMobile) {
       $scope.isMobile = isMobile.isMobile();
       $scope.friday=[
-        {what:'Arrival',
-        when:'08:00',
-        where:'IDC Campus'},
-
-        {what:'Arrival',
+        {
+          what:'Registration',
           when:'08:00',
-          where:'IDC Campus'},
-        {what:'Arrival',
-          when:'08:00',
-          where:'IDC Campus'}
+          where:'IDC Campus'
+        },
+        {
+          what:'Opening Ceremony',
+          when:'09:00',
+          where:'IDC Campus'
+        },
+        {
+          what:'Hacking Starts!',
+          when:'10:00',
+          where:'IDC Campus'
+        },
+        {
+          what:'Lunch',
+          when:'13:00',
+          where:'IDC Campus'
+        },
+        {
+          what:'How To Pitch',
+          when:'15:00',
+          where:'IDC Campus'
+        },
+        {
+          what:'Friday Dinner',
+          when:'19:30',
+          where:'IDC Campus'
+        },
+        {
+          what:'Midnight Meal',
+          when:'00:30',
+          where:'IDC Campus'
+        },
+        {
+          what:'Surprise Snack',
+          when:'03:00',
+          where:'IDC Campus'
+        }
       ];
 
       $scope.saturday = [
-        {what:'Finish',
+        {
+          what:'Breakfast',
+          when:'07:00',
+          where:'IDC Campus'
+        },
+        {
+          what:'Yoga Session',
+          when:'09:30',
+          where:'IDC Campus'
+        },
+        {
+          what:'Lunch',
+          when:'12:00',
+          where:'IDC Campus'
+        },
+        {
+          what:'Snack',
+          when:'16:00',
+          where:'IDC Campus'
+        },
+        {
+          what:'Hacking Ends',
+          when:'17:30',
+          where:'IDC Campus'
+        },
+        {
+          what:'Judging Round 1',
+          when:'18:00',
+          where:'IDC Campus'
+        },
+        {
+          what:'Dinner',
+          when:'19:00',
+          where:'IDC Campus'
+        },
+        {
+          what:'Final Judging and Closing Ceremony',
           when:'20:00',
-          where:'IDC Campus'}
+          where:'IDC Campus'
+        }
       ];
 
     }]);
