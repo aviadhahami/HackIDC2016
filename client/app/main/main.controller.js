b3d83f99c58cfbddf73fdfd11a0ad61a424bfb49
Aviad Hahami <aviadhahami@gmail.com>
2016-05-03 22:10:06 +0300

    putting sched in main page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -130,7 +130,92 @@ angular.module('hackIdcApp')
           content:'The program currently accepts students from IDC Herzliya.. If you wish to participate in the Hackathon and have technical background but little to no experience, this program is for you.<br><br>Just check the “I want to be part of the mentorship program” box when submitting the registration form. We will contact you once the application results are announced.<br><br>Be advised that you will be required to attend 5 to 6 weekly sessions in the weeks preceding the Hackathon. We expect you to meet this requirement so we can advance your skills to a Hackathon Hacker level.'
         }
       ];
+      $scope.friday=[
+        {
+          what:'Registration',
+          when:'08:00',
+          where:'Psychology Building Lobby'
+        },
+        {
+          what:'Opening Ceremony',
+          when:'09:00',
+          where:'PEB20'
+        },
+        {
+          what:'Hacking Starts!',
+          when:'10:00',
+          where:'Psychology Building'
+        },
+        {
+          what:'Lunch',
+          when:'13:00',
+          where:'Outside Psychology Building'
+        },
+        {
+          what:'How To Pitch',
+          when:'15:00',
+          where:'PEB20'
+        },
+        {
+          what:'Friday Dinner',
+          when:'19:30',
+          where:'Outside Psychology Building'
+        }
+      ];
 
+      $scope.saturday = [
+
+        {
+          what:'Midnight Meal',
+          when:'00:30',
+          where:'Outside Psychology Building'
+        },
+        {
+          what:'Surprise Snack',
+          when:'03:00',
+          where:'Psychology Building Lobby'
+        },
+        {
+          what:'Breakfast',
+          when:'07:00',
+          where:'Outside Psychology Building'
+        },
+        {
+          what:'Yoga Session',
+          when:'09:30',
+          where:'Outside Arison Building'
+        },
+        {
+          what:'Lunch',
+          when:'12:00',
+          where:'Outside Psychology Building'
+        },
+        {
+          what:'Snack',
+          when:'16:00',
+          where:'Outside Psychology Building'
+        },
+        {
+          what:'Hacking Ends',
+          when:'17:30',
+          where:'Psychology Building'
+        },
+        {
+          what:'Judging Round 1',
+          when:'18:00',
+          where:'Psychology Building'
+        },
+        {
+          what:'Dinner',
+          when:'19:00',
+          where:'Outside Psychology Building'
+        },
+        {
+          what:'Final Judging and Closing Ceremony',
+          when:'20:00',
+          where:'PEB20'
+        }
+      ];
 
       $scope.titles = {
         mainTitle : 'HackIDC 2016',
