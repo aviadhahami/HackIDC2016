487e52502ff9f29ea830b70e06a42a65609e4450
Aviad Hahami <aviadhahami@gmail.com>
2016-05-06 09:42:54 +0300

    fixed table

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -130,11 +130,12 @@ angular.module('hackIdcApp')
           content:'The program currently accepts students from IDC Herzliya.. If you wish to participate in the Hackathon and have technical background but little to no experience, this program is for you.<br><br>Just check the “I want to be part of the mentorship program” box when submitting the registration form. We will contact you once the application results are announced.<br><br>Be advised that you will be required to attend 5 to 6 weekly sessions in the weeks preceding the Hackathon. We expect you to meet this requirement so we can advance your skills to a Hackathon Hacker level.'
         }
       ];
+
       $scope.friday=[
         {
           what:'Registration',
           when:'08:00',
-          where:'Psychology Building Lobby'
+          where:'Lobby'
         },
         {
           what:'Opening Ceremony',
@@ -149,7 +150,7 @@ angular.module('hackIdcApp')
         {
           what:'Lunch',
           when:'13:00',
-          where:'Outside Psychology Building'
+          where:'Outside'
         },
         {
           what:'How To Pitch',
@@ -159,56 +160,57 @@ angular.module('hackIdcApp')
         {
           what:'Friday Dinner',
           when:'19:30',
-          where:'Outside Psychology Building'
+          where:'Outside'
+        },
+
+        {
+          what:'Midnight Meal',
+          when:'00:30',
+          where:'Outside'
+        },
+        {
+          what:'Surprise Snack',
+          when:'03:00',
+          where:'Lobby'
         }
       ];
 
       $scope.saturday = [
 
         {
-          what:'Midnight Meal',
-          when:'00:30',
-          where:'Outside Psychology Building'
-        },
-        {
-          what:'Surprise Snack',
-          when:'03:00',
-          where:'Psychology Building Lobby'
-        },
-        {
           what:'Breakfast',
           when:'07:00',
-          where:'Outside Psychology Building'
+          where:'Outside'
         },
         {
           what:'Yoga Session',
           when:'09:30',
-          where:'Outside Arison Building'
+          where:'Arison Building'
         },
         {
           what:'Lunch',
           when:'12:00',
-          where:'Outside Psychology Building'
+          where:'Outside'
         },
         {
           what:'Snack',
           when:'16:00',
-          where:'Outside Psychology Building'
+          where:'Outside'
         },
         {
           what:'Hacking Ends',
           when:'17:30',
-          where:'Psychology Building'
+          where:''
         },
         {
           what:'Judging Round 1',
           when:'18:00',
-          where:'Psychology Building'
+          where:''
         },
         {
           what:'Dinner',
           when:'19:00',
-          where:'Outside Psychology Building'
+          where:'Outside'
         },
         {
           what:'Final Judging and Closing Ceremony',
